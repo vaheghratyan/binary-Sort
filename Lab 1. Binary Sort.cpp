975d5ab596204bf7d5e52ceefed8457c0ae2d637
vaheghratyan <vaheghratyan@gmail.com>
2023-11-13 18:23:55 +0300

    first commit

---
+++ Lab 1. Binary Sort.cpp
@@ -0,0 +1,42 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+	int count, b[30], buf;
+
+	printf("Enter the number of elements ");
+	scanf("%d", &count);
+
+	printf("Enter the elements of array\n");
+	for (int i = 0; i < count; i++)
+		scanf("%d", &b[i]);
+
+
+	for (int i = 1; i < count; i++)
+		if (b[i] < b[i - 1])
+		{
+			buf = b[i];
+			int left = 0;
+			int right = i - 1;
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+				if (buf > b[mid])
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+			for (int j = i - 1; j > right; j--)
+				b[j + 1] = b[j];
+			b[right + 1] = buf;
+		}
+
+	for (int i = 0; i < count; i++)
+		printf("%d ", b[i]);
+
+
+	system("pause");
+	return 0;
+}
